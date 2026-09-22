b6ce8ee35de2ff478f1152afab296ace3cf879d7
Koné Mohamed <konemhd2003@gmail.com>
2026-09-22 02:32:50 +0200

    Delete Python directory

--- Python/calculateur_de_frais_des_ports.py
+++
@@ -1,6 +0,0 @@
-panier=float(input("Entrez le montant de votre panier: "))
-if (panier>=50) :
-    print(f"Livraison offerte ! Le montant total de la commande est : {panier} £")
-else :
-    panier_avec_frais= panier + 4.90
-    print(f"Le montant total de la commande est : {panier_avec_frais} £")
--- Python/conversion_degre.py
+++
@@ -1,3 +0,0 @@
-c=float(input("Entrez la valeur du degre celsius : "))
-f=(c*1.8)+32
-print("La température en Fahrenheit = ",f)
